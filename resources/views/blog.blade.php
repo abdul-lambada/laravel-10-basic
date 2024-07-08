dcf7a3ffe0477d2dc2d305781f1f10df1610d326
Abdul Kholik <engineertekno@gmail.com>
2024-07-08 14:36:38 +0700

    Route dan View Pada Laravel

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel-"10"</title>
+</head>
+<body>
+
+    <h3>ww.laravel.com</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eius aut placeat, quae ea totam molestiae deserunt soluta laborum officiis!</p>
+    <p>Ini adalah halaman blog Laravel</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//membuat route terbaru dengan get tanpa view
+Route::get('halo', function () {
+    return "Hallo, Selamat Datang di Laravel 10";
+});
+
+// Route get dengan view
+Route::get('blog', function() {
+    return view('blog');
+});
